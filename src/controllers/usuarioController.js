e260a3bd39d57b43232b26b597c36d48912e3678
Wendel <wendel824829has@gmail.com>
2024-07-29 10:10:33 -0300

    validacao e cadastro de usuario finalizados

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,55 @@
-import { response } from "express";
+import conn from "../config/conn.js";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 
 export const register = (request, response) => {
-    response.send("Controlador de usuário")
-}
+  const { nome, email, telefone, senha, confirmsenha } = request.body;
+
+  const checkEmailSQL = /*sql*/ `SELECT * FROm usuarios WHERE ?? = ?`;
+  const checkEmailData = ["email", email];
+  conn.query(checkEmailSQL, checkEmailData, async (err, data) => {
+    if (err) {
+      console.log(err);
+      response.status(500).json({ err: "Não foi possível buscar usuario" });
+    }
+    if (data.length > 0) {
+      response.status(409).json({ err: "E-mail já está em uso!" });
+    }
+    const salt = await bcrypt.genSalt(12);
+    const senhaHash = await bcrypt.hash(senha, salt);
+    // console.log(salt);
+    // console.log("senha recebida: ", senha);
+    // console.log("Senha Criptografada: ", senhaHash);
+
+    // Cadastrar Usuário
+    const id = uuidv4();
+    const imagem = "userDefault.png";
+
+    const insertSql = /*sql*/ `INSERT INTO usuarios
+    (??, ??, ??, ??, ??, ??) Values (?, ?, ?, ?, ?, ?)`;
+
+    const insertData = [
+      "usuario_id",
+      "nome",
+      "email",
+      "telefone",
+      "senha",
+      "imagem",
+      id,
+      nome,
+      email,
+      telefone,
+      senhaHash,
+      imagem,
+    ];
+    conn.query(insertSql, insertData, (err) => {
+      if (err) {
+        console.error(err);
+        response.status(500).json({ err: "Erro ao cadastrar usuário" });
+        return;
+      }
+
+      response.status(201).json({ message: "Usuário cadastrado" });
+    });
+  });
+};
--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -3,9 +3,12 @@ import { response, Router } from "express";
 //importar Controllers de usuario
 import { register } from "../controllers/usuarioController.js"
 
+//Importar os helpers
+import validarUsuario from "../helpers/validar-user.js";
+
 const router = Router();
 
 //localhost:3333/usuarios/register
-router.post("/register", register);
+router.post("/register", validarUsuario, register);
 
 export default router;
