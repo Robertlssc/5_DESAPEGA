1c34599752732019378424f42e7e22eaaf666ec2
Wendel <wendel824829has@gmail.com>
2024-07-29 08:29:23 -0300

    Rota 404 criada

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import "dotenv/config";
 
-const app = express();
-
 const PORT = process.env.PORT;
 
 //importar conexão
 import conn from "./config/conn.js";
 
-app.get("/", (request, response) => {
-  response.send("Olá, Mundo!");
-});
+const app = express();
+
+app.use((request, response) => {
+  response.status(404).json({ message: "Recurso não encontrado " });
+}); //404
+
+// app.get("*", (request, response) => {
+//   response.send("Olá, Mundo!");
+// });
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta: " + PORT);
